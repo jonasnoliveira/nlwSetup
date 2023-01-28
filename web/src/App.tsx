bc506f2c573bab82434099739a333728d82e5495
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-01-28 17:55:30 -0300

    Mobile configured

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/global.css'
+import {Habit} from './components/Habit'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Habit completed={3}/>
+    </div>
   )
 }
 
