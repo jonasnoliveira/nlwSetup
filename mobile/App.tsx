f559be30e40b3ce2a9dbeaf3e2b1c154996089df
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-01-29 17:21:55 -0300

    Update

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Hello React Native
+        Habit
       </Text>
       <Button title='Jonas'/>
       <StatusBar style="auto" />
