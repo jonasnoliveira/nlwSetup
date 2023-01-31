e09212d37f21842a5d072bf401ed5435140577e5
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-01-30 23:21:34 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import './styles/global.css'
 import {Habit} from './components/Habit'
 
+const img = 'https://repository-images.githubusercontent.com/589784841/4ca0d44d-589f-4394-9f79-dbfa6c235b03'
+
 function App() {
   return (
     <div>
       <Habit completed={3}/>
+      <img src={img}/>
     </div>
   )
 }
